ef10d9bb3cf5a38d996c9f345455ded449c6eba0
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-06 18:48:16 +0200

    Commit Ejercicio4

---
+++ TA04_Ejercicio4/src/Ejercicio4App.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio4App {
+
+	public static void main(String[] args) {
+		
+		int N= 7;
+		int incrementar = N+77;
+		int decrementar = incrementar - 3;
+		System.out.println("El valor de N es ="+N);
+		System.out.println("El valor de incrementar N en 77 es ="+incrementar);
+		System.out.println("El valor de decrementar  en 3 es ="+decrementar);
+		System.out.println("El valor de duplicar  es ="+(decrementar*2));
+	}
+
+}
