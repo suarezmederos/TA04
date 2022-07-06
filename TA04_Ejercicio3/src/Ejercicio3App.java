8fcef4770cab1463ad6b309d43f459b083e70698
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-06 18:46:17 +0200

    Commit Ejercicio 3

---
+++ TA04_Ejercicio3/src/Ejercicio3App.java
@@ -0,0 +1,38 @@
+
+public class Ejercicio3App {
+	
+	public static void main(String[] args) {
+		
+		int X=10;
+		int Y=9;
+		double N=8.2;
+		double M= 7.3;
+		
+		double operacion= 0;
+		
+		System.out.println("El valor de la variable X="+X);
+		System.out.println("El valor de la variable Y="+Y);
+		System.out.println("El valor de la variable N="+N);
+		System.out.println("El valor de la variable M="+M);
+		System.out.println("El valor de la suma de X e Y es ="+(operacion=X+Y));
+		System.out.println("El valor de la diferencia de X e Y es ="+(operacion=X-Y));
+		System.out.println("El valor del producto de X e Y es ="+(operacion=X*Y));
+		System.out.println("El valor del coeficiente de X e Y es ="+(operacion=X/Y));
+		System.out.println("El valor del resto de X e Y es ="+(operacion=X%Y));
+		System.out.println("El valor de la suma de N y M es ="+(operacion=N+M));
+		System.out.println("El valor de la diferencia de N y M es ="+(operacion=N-M));
+		System.out.println("El valor del producto de N y M es ="+(operacion=N*M));
+		System.out.println("El valor del coeficiente de N y M es ="+(operacion=N/M));
+		System.out.println("El valor del resto de N y M es ="+(operacion=N%M));
+		System.out.println("El valor de la suma de X y N es ="+(operacion=X+N));
+		System.out.println("El valor del coeficiente de Y y M es ="+(operacion=Y/M));
+		System.out.println("El valor del resto de Y y M es ="+(operacion=Y%M));
+		System.out.println("El doble de X es ="+(operacion=X*2));
+		System.out.println("El doble de Y es ="+(operacion=Y*2));
+		System.out.println("El doble de N es ="+(operacion=N*2));
+		System.out.println("El doble de M es ="+(operacion=M*2));
+		System.out.println("La suma de todas las variables es ="+(operacion=X+Y+N+M));
+		System.out.println("El producto de todas las variables es ="+(operacion=X*Y*N*M));
+	}
+
+}
