749e69bf2264b5b617d6b9af009f1009885d86e9
jmiguel.suarez <jmiguel@automaticaintegral.com>
2022-07-06 18:38:42 +0200

    Commint del ejercicio1

---
+++ TA04_Ejercicio1/src/Ejercicio1App.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio1App {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 4;
+		
+		int suma = a+b;
+		int resta = a-b;
+		int multiplicacion = a*b;
+		double resto = a%b;
+		
+		System.out.println("El valor de la suma de a y b es:"+suma);
+		System.out.println("El valor de la resta de a y b es:"+resta);
+		System.out.println("El valor de la multiplicar a y b es:"+multiplicacion);
+		System.out.println("El valor del resto entre a y b es:"+resto);
+	}
+
+}
